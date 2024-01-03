f23a25e3d4fad79de12eb99003ed28ab97192b5c
Andreas Isidorsson <isidorssona@gmail.com>
2024-01-03 13:59:24 +0100

    Refactor PortfolioItem component

--- src/components/PortfolioItems.jsx
+++ src/components/PortfolioItems.jsx
@@ -20,8 +20,7 @@ const handleMouseLeave = (event) => {
   event.currentTarget.style.transform = "";
 };
 
-function PortfolioItem({ title, description, imgBg, imgUrl,  stack = [], link }) {
-  
+function PortfolioItem({ title, description, imgBg, imgUrl, stack = [], link, }) {
   const gradientColors = stack.map((item) => item.color).join(", ");
 
   const [isOpen, setIsOpen] = useState(false);
@@ -36,9 +35,6 @@ function PortfolioItem({ title, description, imgBg, imgUrl,  stack = [], link })
       // onMouseLeave={handleMouseLeave}
       style={{
         backgroundImage: `url(${imgBg})`,
-        // backgroundSize: "cover",
-        // backgroundPosition: "center",
-        // transition: "transform 0.5s ease-out",
       }}
       target="_blank"
       rel="noreferrer"
@@ -142,25 +138,18 @@ function PortfolioItem({ title, description, imgBg, imgUrl,  stack = [], link })
           <dt className="text-sm font-medium text-black dark:text-gray-400"></dt>
           <dd className="text-xs text-black dark:text-gray-400"></dd>
         </div>
-        
-        <div>
-          <a
-            className="absolute bottom-5 left-5 inline-flex items-center text-blue-600 dark:text-blue-600 hover:underline"
-            href={link}
-            target="_blank"
-            rel="noopener noreferrer"
+
+        <div className="absolute bottom-5 left-5 inline-flex items-center text-blue-600 dark:text-blue-600 hover:underline">
+          Link to project
+          <svg
+            className="w-5 h-5 ml-2"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            {/* Link */}
-            <svg
-              className="w-5 h-5 ml-2"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
-              <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
-            </svg>
-          </a>
+            <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path>
+            <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path>
+          </svg>
         </div>
       </dl>
     </a>
