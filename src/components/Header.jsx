0d56076d620315e04837e597c618f062a7466cd8
Andreas Isidorsson <isidorssona@gmail.com>
2024-01-03 11:53:24 +0100

    Refactor header information

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,17 +43,18 @@ function Information() {
         alt="Andreas Isidorsson"
         className="animate-fade-down w-full h-36 md:w-full md:h-60 object-cover mt-4 border-4 border-cyan-600 dark:border-cyan-600 absolute z-10 filter blur-sm"
       />
-<h1 className="animate-fade-down text-4xl md:text-7xl text-gray-200 dark:text-gray-200 mb-1 md:mb-5 font-Raleway tracking-tight z-20 relative top-6 ">
-  Andreas Isidorsson
-</h1>
+      <h1 className="animate-fade-down text-4xl md:text-7xl text-gray-200 dark:text-gray-200 mb-1 md:mb-5 font-Raleway tracking-tight z-20 relative top-6 ">
+        Andreas Isidorsson
+      </h1>
       <p className="animate-fade-down text-1xl md:text-2xl mb-3 text-gray-200 dark:text-gray-200 font-Raleway z-20 relative top-3 md:top-1">
         A developer that enjoy coding and gaming.
       </p>
       <p className="animate-fade-down max-w-xl mb-4 text-sm md:text-xl text-gray-200 dark:text-gray-200 font-Raleway tracking-tight z-20 relative top-3 md:top-1">
-        I'm a self-taught developer deeply enthusiastic about technology and gaming. Over the past three years, I've honed my coding skills, gaining experience across a diverse spectrum of technologies and programming languages.
+        A self-taught developer with a passion for technology and gaming, I've
+        spent the past years honing my coding skills across a wide range of
+        technologies and programming languages.
       </p>
     </div>
   );
 }
 export default Information;
-
